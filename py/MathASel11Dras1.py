1173918296810fc4b26f8461d2ccea9dbeb1b2dd
dimnikolos <dnikolos@gmail.com>
2018-02-09 12:36:59 +0200

    print min max and messages

--- py/MathASel11Dras1.py
+++ py/MathASel11Dras1.py
@@ -21,12 +21,15 @@ if x>=100 and x<=999:
     x = x - psifio1
     x = x // 10
     psifio2 = x
-
+  
     enallages = []
     for enallagi in itertools.permutations([psifio0,psifio1,psifio2]):
         arithmos = 100*enallagi[2]+10*enallagi[1]+enallagi[0]
         if arithmos not in enallages:
             enallages.append(arithmos)
+    print('Ο μεγαλύτερος αριθμός είναι: ' + str(max(enallages)))
+    print('Ο μικρότερος αριθμός είναι: ' + str(min(enallages)))
+    print('Όλοι οι αριθμοί με φθίνουσα σειρά:')
     for arithmos in sorted(enallages,reverse = True):
         print(arithmos)
 else:
